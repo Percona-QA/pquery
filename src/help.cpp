cf596ff7343796f5f0861a5f6b8060ea624066f3
Roel Van de Paar <roel.vandepaar@percona.com>
2016-09-09 11:52:48 +1000

    Help text update

--- src/help.cpp
+++ src/help.cpp
@@ -11,10 +11,9 @@ print_version(void) {
 void
 show_help() {
   print_version();
-  std::cout << " * Please see specific help:" << std::endl;
-  std::cout << " pquery --config-help for INI-config help, MULTIPLE nodes mode" << std::endl;
-  std::cout << " pquery --cli-help for commandline options, SINGLE node mode" << std::endl;
-  std::cout << "=> pquery doesn't support multiple nodes when using commandline options mode!" << std::endl;
+  std::cout << " * pquery supports two different use/configuration modes; commandline or INI-file. Please see specific help:" << std::endl;
+  std::cout << "   pquery --config-help for INI-config help, this mode supports SINGLE and MULTIPLE node(s)" << std::endl;
+  std::cout << "   pquery --cli-help for commandline options, this mode supports a SINGLE node only" << std::endl;
   }
 
 
