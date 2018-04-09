29b9cb694fc043577e4cb9141b5f634dfbd7da2f
Alexey Bychko <abychko@gmail.com>
2018-04-09 22:02:18 +0700

    added cmake PostgreSQL module into config

--- format.sh
+++ format.sh
@@ -1,9 +1,16 @@
 #/usr/bin/env bash
-
+#
+if [ -z $(which bcpp) ]; then
+  echo "* BCPP is not installed!"
+  exit 1
+fi
+#
+#
 cd $(dirname ${0})
-
+#
 for _file in $(find . -type f -iname '*.cpp' -o -iname '*.hpp' -o -iname '*.c' -o -iname '*.h'); do
   echo "- formatting ${_file}"
   bcpp -bcl -i 2 -s -tbcl -ybbi ${_file} ${_file}.new
   mv -f ${_file}.new ${_file}
 done
+#
